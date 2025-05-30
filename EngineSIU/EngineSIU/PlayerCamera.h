a16ad43f6ee9abc9bbac1484d13e76037c9d5a30
cjs1744 <cjs1744@gmail.com>
2025-05-31 03:45:48 +0900

    카메라 셔터효과

--- EngineSIU/EngineSIU/PlayerCamera.h
+++ EngineSIU/EngineSIU/PlayerCamera.h
@@ -9,11 +9,20 @@ public:
 
 private:
     TArray<FRenderTargetRHI*> Pictures;
+    bool  bIsShutterAnimating = false;
+    float ShutterTimer = 0.0f;
+    float ShutterDuration = 0.25f; // 예: 0.25초
+    float CurrentApertureProgress = 0.0f;
 
 public:
     void TakePicture();
     void ReleasePictures();
 
     TArray<FRenderTargetRHI*> GetPictures();
+    void TriggerShutterEffect();
+    void UpdateShutterAnimation(float deltaTime);
+
+    float GetCurrentApertureProgress();
+    void SetCurrentApertureProgress(float value);
 };
 
