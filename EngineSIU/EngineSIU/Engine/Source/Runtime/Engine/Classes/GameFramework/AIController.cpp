5b3765ca5e877458255eb6694e1681cc89d98698
kbjun98 <110660221+kbjun98@users.noreply.github.com>
2025-06-01 02:35:38 +0900

    wip: AIController 작업 중

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/AIController.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/AIController.cpp
@@ -1,1 +1,85 @@
 #include "AIController.h"
+#include "Engine/Contents/Actors/GridMapActor.h"
+#include "Engine/Contents/Navigation/PathFinder.h"
+#include "Engine/Contents/Navigation/MapData.h"
+#include "Engine/Contents/Components/GridMapComponent.h"
+
+void AAIController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    if (IsPathValid())
+    {
+        MoveAlongPath(DeltaTime);
+    }
+}
+
+void AAIController::MoveTo(const FVector& InDestination)
+{
+    Destination = InDestination;
+    AGridMapActor* GridMapActor = GetControlledGridMapActor();
+
+    int StartX = static_cast<int>(GridMapActor->GetActorLocation().X);
+    int StartY = static_cast<int>(GridMapActor->GetActorLocation().Y);
+
+    int TargetX = static_cast<int>(Destination.X);
+    int TargetY = static_cast<int>(Destination.Y);
+
+    FGridNode& StartNode = GridMapActor->GridComponent->GetNode(StartX,StartY);
+    FGridNode& TargetNode = GridMapActor->GridComponent->GetNode(TargetX, TargetY);
+
+    FPathFinder* PathFinder = GridMapActor->GridComponent->PathFinder;
+    //CurrentPath = PathFinder->FindPath(GridMapActor->GridComponent->GridMap, StartNode, TargetNode);
+
+    if (CurrentPath.IsEmpty())
+    {
+        UE_LOG(ELogLevel::Display, "No path found to destination.");
+    }
+    else
+    {
+        CurrentPathIndex = 0;
+        UE_LOG(ELogLevel::Display, "Path found. Starting to move.");
+    }
+}
+
+void AAIController::OnMoveCompleted()
+{
+}
+
+void AAIController::MoveAlongPath(float DeltaTime)
+{
+    if (CurrentPathIndex >= CurrentPath.Num())
+    {
+        UE_LOG(ELogLevel::Display, "Reached the end of the path.");
+        return;
+    }
+
+    AGridMapActor* GridMapActor = GetControlledGridMapActor();
+
+    FGridNode* TargetNode = CurrentPath[CurrentPathIndex];
+    FVector TargetLocation = GridMapActor->GridComponent->GridMap.GetWorldPosition(TargetNode->X, TargetNode->Y);
+
+    FVector CurrentLocation = GridMapActor->GetActorLocation();
+    FVector Direction = (TargetLocation - CurrentLocation).GetSafeNormal();
+    FVector NewLocation = CurrentLocation + Direction * MovementSpeed * DeltaTime;
+
+    GridMapActor->SetActorLocation(NewLocation);
+
+    if (FVector::Dist(NewLocation, TargetLocation) < CellTolerance) // 10 units tolerance
+    {
+        CurrentPathIndex++;
+        if (CurrentPathIndex >= CurrentPath.Num())
+        {
+            OnMoveCompleted();
+        }
+    }
+}
+
+bool AAIController::IsPathValid() const
+{
+    return CurrentPath.Num() > 0 && CurrentPathIndex < CurrentPath.Num();
+}
+
+AGridMapActor* AAIController::GetControlledGridMapActor() const
+{
+    return Cast<AGridMapActor>(ControlledActor);
+}
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/AIController.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/AIController.h
@@ -1,10 +1,31 @@
 #pragma once
 #include "Actor.h"
+
+class FGridNode;
+class AGridMapActor;
 class AAIController : public AActor
 {
     DECLARE_CLASS(AAIController, AActor)
 public:
     AAIController() = default;
-    virtual ~AAIController() override = default;
+    virtual ~AAIController() = default;
+    virtual void Tick(float DeltaTime) override;
+    virtual void MoveTo(FVector& InDestination);
+    virtual void OnMoveCompleted();
+protected:
+    FVector Destination;
+    TArray<FGridNode*> CurrentPath; // 경로를 저장하는 배열
+    int32 CurrentPathIndex = 0; // 현재 경로의 인덱스
+
+protected:
+    void MoveAlongPath(float DeltaTime);
+    bool IsPathValid() const;
+
+protected:
+    AActor* ControlledActor = nullptr; // 이 AI가 제어하는 Actor
+    AGridMapActor* GetControlledGridMapActor() const;
+protected:
+    float MovementSpeed = 100.0f; // 이동 속도
+    float CellTolerance = 1.0f;
 };
 
